347ec3f4df2cb5098e67a126de3472a45df9a6c1
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-10-09 13:23:07 +0300

    initial commit

---
+++ tests/AbstractTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Nip\Cookie\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AbstractTest
+ */
+abstract class AbstractTest extends TestCase
+{
+    protected $object;
+
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+}
